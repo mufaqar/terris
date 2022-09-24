6ee832962fbfc0d484cf166a1b497d5eab689953
Mufaqar Islam <mufaqar@gmail.com>
2022-09-24 16:30:19 +0500

    images

--- components/workBox.js
+++ components/workBox.js
@@ -1,20 +1,21 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
-import Project1 from '../public/images/project1.png';
 
 export default function WorkBox(props) {
-    return (
-        <div className='pb-10 pt-5'>
-            <Link href='/works/single-project'><a>
-                <figure>
-                    <Image src={Project1} alt=''></Image>
-                </figure>
-                <div className='md:text-[1.375rem] text-[0.813rem]'>
-                    <h3 className='font-medium'>{props.title}</h3>
-                    <p className='font-normal'>{props.detail}</p>
-                    <p className='font-normal'>{props.info}</p>
-                </div>
-                </a></Link>
-        </div>
-    )
+  return (
+    <div className="pb-10 pt-5">
+      <Link href="/works/single-project">
+        <a>
+          <figure className="relative">
+            <Image src={props.icon} layout="fill" alt=""></Image>
+          </figure>
+          <div className="md:text-[1.375rem] text-[0.813rem]">
+            <h3 className="font-medium">{props.title}</h3>
+            <p className="font-normal">{props.detail}</p>
+            <p className="font-normal">{props.info}</p>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
 }
--- pages/works/index.js
+++ pages/works/index.js
@@ -1,17 +1,38 @@
-import WorkBox from '../../components/workBox'
+import WorkBox from '../../components/workBox';
 
 export default function Index() {
-    return (
-        <>
-            <section>
-                <div className='conteiner md:w-7/12 w-full mx-auto px-2'>
-                    <WorkBox title="Union Street" detail="MA+HG" info="Restoration + In-fill Building"/>
-                    <WorkBox title="Union Street" detail="MA+HG" info="Restoration + In-fill Building"/>
-                    <WorkBox title="Union Street" detail="MA+HG" info="Restoration + In-fill Building"/>
-                    <WorkBox title="Union Street" detail="MA+HG" info="Restoration + In-fill Building"/>
-                    <WorkBox title="Union Street" detail="MA+HG" info="Restoration + In-fill Building"/>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section>
+        <div className="conteiner md:w-7/12 w-full mx-auto px-2">
+          <WorkBox
+            title="Union Street"
+            detail="MA+HG"
+            info="Restoration + In-fill Building"
+            icon="/images/union/2-Union Street-Terris And Company-MAHG-Infill Building.webp"
+          />
+          <WorkBox
+            title="Union Street"
+            detail="MA+HG"
+            info="Restoration + In-fill Building"
+          />
+          <WorkBox
+            title="Union Street"
+            detail="MA+HG"
+            info="Restoration + In-fill Building"
+          />
+          <WorkBox
+            title="Union Street"
+            detail="MA+HG"
+            info="Restoration + In-fill Building"
+          />
+          <WorkBox
+            title="Union Street"
+            detail="MA+HG"
+            info="Restoration + In-fill Building"
+          />
+        </div>
+      </section>
+    </>
+  );
 }
