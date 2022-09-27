9dde0360034ae6aff8c27c38424a0bed106817f1
Mufaqar Islam <mufaqar@gmail.com>
2022-09-27 11:59:56 +0500

    word padding

--- components/workBox.js
+++ components/workBox.js
@@ -1,23 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function WorkBox({featureImage,title,tag,type,slug}) {
+export default function WorkBox({ featureImage, title, tag, type, slug }) {
   return (
-    <div className="pb-10">
+    <div className="pb-[42px]">
       <Link href={`/works/${slug}`}>
         <a>
           <figure className="relative work">
-          <Image
+            <Image
               src={featureImage?.asset.url}
               layout="fill"
               alt={featureImage?.caption}
               className="work-featured"
             ></Image>
           </figure>
-          <div className="mt-4">
-            <h3 className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskMeduim">{title}</h3>
-            <p className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">{tag}</p>
-            <p className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">{type}</p>
+          <div className="mt-[9px]">
+            <h3 className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskMeduim">
+              {title}
+            </h3>
+            <p className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+              {tag}
+            </p>
+            <p className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+              {type}
+            </p>
           </div>
         </a>
       </Link>
