33a2b50573f5e0da74a334c26f02f11c14d698ca
Mufaqar Islam <mufaqar@gmail.com>
2022-09-27 13:29:20 +0500

    about space

--- components/teamBox.js
+++ components/teamBox.js
@@ -1,19 +1,35 @@
-import OwnImage from './OwnImage'
+import OwnImage from './OwnImage';
 
-export default function TeamBox({name, detail, imgPath, teamState, teamOpen, id}) {
-  
-  const handleClick=(id)=>{
-    if(teamOpen === id){
-      return teamState(null)
+export default function TeamBox({
+  name,
+  detail,
+  imgPath,
+  teamState,
+  teamOpen,
+  id,
+}) {
+  const handleClick = (id) => {
+    if (teamOpen === id) {
+      return teamState(null);
     }
-    teamState(id)
-  }
-  
+    teamState(id);
+  };
+
   return (
     <>
-      <div className="relative cursor-pointer group" onClick={()=>handleClick(id)}>
-        <OwnImage path={imgPath?.asset.url} alt={imgPath?.asset.caption ? imgPath?.asset.caption : name}></OwnImage>
-        <div className={`absolute inset-0 grid justify-between p-1 overflow-auto bg-white  team-overlay ${ teamOpen === id ? 'opacity-100': 'opacity-0'}`} >
+      <div
+        className="relative cursor-pointer group"
+        onClick={() => handleClick(id)}
+      >
+        <OwnImage
+          path={imgPath?.asset.url}
+          alt={imgPath?.asset.caption ? imgPath?.asset.caption : name}
+        ></OwnImage>
+        <div
+          className={`absolute inset-0 grid content-start p-0 overflow-auto bg-white  team-overlay ${
+            teamOpen === id ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
           <h3 className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskMeduim mb-2">
             {name}
           </h3>
