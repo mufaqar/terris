c759dc4c6a4a9620bcb0230590f3886c698edf71
Mufaqar Islam <mufaqar@gmail.com>
2022-09-27 12:28:02 +0500

    menu

--- components/header.js
+++ components/header.js
@@ -17,7 +17,7 @@ export default function Header() {
       </Head>
 
       <nav className="">
-        <div className="container md:w-7/12 w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal pt-[21px] pb-[2.625rem] px-2 flex justify-between">
+        <div className="container lg:w-7/12 w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal pt-[21px] pb-[2.625rem] px-2 flex justify-between">
           <NavLink to="/about">About</NavLink>
           <NavLink to="/">Terris & Company</NavLink>
           <NavLink to="/contact">Contact</NavLink>
