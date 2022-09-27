f4054381ae6a3575d2167c02709feddff5def10a
Mufaqar Islam <mufaqar@gmail.com>
2022-09-27 11:50:23 +0500

    move to live

--- components/header.js
+++ components/header.js
@@ -1,34 +1,28 @@
 import Head from 'next/head';
 
 function NavLink({ to, children }) {
-    return <a href={to} className={``}>
-        {children}
+  return (
+    <a href={to} className={``}>
+      {children}
     </a>
+  );
 }
 
 export default function Header() {
+  return (
+    <>
+      <Head>
+        <title>Terris & Company</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
 
-    return (
-        <>
-            <Head>
-                <title>Terris & Company</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </Head>
-
-            <nav className="">
-                <div className="container md:w-7/12 w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal pt-16 pb-[2.625rem] px-2 flex justify-between">
-                    <NavLink to="/about">
-                        About
-                    </NavLink>
-                    <NavLink to="/">
-                        Terris & Company
-                    </NavLink>
-                    <NavLink to="/contact">
-                        Contact
-                    </NavLink>
-                </div>
-            </nav>
-
-        </>
-    )
+      <nav className="">
+        <div className="container md:w-7/12 w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal pt-8 pb-[2.625rem] px-2 flex justify-between">
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/">Terris & Company</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+      </nav>
+    </>
+  );
 }
