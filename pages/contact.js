c4a61fd8596144aa16d53ebb7f42af2856fdd190
Mufaqar Islam <mufaqar@gmail.com>
2022-09-27 12:11:47 +0500

    contact us

--- pages/contact.js
+++ pages/contact.js
@@ -1,39 +1,63 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Contact() {
-    return (
-        <>
-            <section>
-                <div className='container md:w-7/12 w-full mx-auto md:pt-10 md:pb-5 py-8 px-2 h-[80vh] flex flex-col justify-between'>
-                    <div className='grid gap-7'>
-                        <ul className=''>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>Terris & Company General Contractors</li>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>facilitating design intent™</li>
-                        </ul>
+  return (
+    <>
+      <section>
+        <div className="container md:w-7/12 w-full mx-auto  md:pb-5  px-2 h-[80vh] flex flex-col justify-between">
+          <div className="grid gap-7">
+            <ul className="">
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                Terris & Company General Contractors
+              </li>
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                facilitating design intent™
+              </li>
+            </ul>
 
-                        <ul className=''>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>853 Union Street</li>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>Vancouver, BC</li>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>Canada V6A 2C5</li>
-                        </ul>
+            <ul className="">
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                853 Union Street
+              </li>
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                Vancouver, BC
+              </li>
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                Canada V6A 2C5
+              </li>
+            </ul>
 
-                        <ul className=''>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>
-                                <Link href='#'><a>+1 604 535 5323</a></Link>
-                            </li>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>
-                                <Link href='#'><a>project@terrisco.com</a></Link>
-                            </li>
-                        </ul>
+            <ul className="">
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                <a href="tell:+1 604 535 5323">
+                  <a>+1 604 535 5323</a>
+                </a>
+              </li>
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                <a href="mailto:rterris@terrisco.com">
+                  <a>rterris@terrisco.com</a>
+                </a>
+              </li>
+            </ul>
 
-                        <ul className=''>
-                            <li className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>Instagram</li>
-                        </ul>
-                    </div>
-                    <p className='lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular'>A nice one by Maker Brothers Worldwide®</p>
-                </div>
-            </section>
-        </>
-    )
+            <ul className="">
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                <a href="https://www.instagram.com/terris.co" target="_blank">
+                  Instagram
+                </a>
+              </li>
+            </ul>
+            <ul className="">
+              <li className="lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular">
+                <a href="https://www.makerbros.co" target="_blank">
+                  A nice one by Maker Brothers Worldwide®
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
