434e7c5372ded00fb92a27891a0566b4361ed1f5
Mufaqar Islam <mufaqar@gmail.com>
2022-11-30 21:30:32 +0500

    margin

--- components/header.js
+++ components/header.js
@@ -16,8 +16,8 @@ export default function Header() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <nav className="mb-[82px]">
-        <div className="xl:w-[70%] w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal pt-[21px] px-2 flex justify-between">
+      <nav className="mb-[84px]">
+        <div className="xl:w-[70%] w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal mt-[9px] px-2 flex justify-between">
           <NavLink to="/about">About</NavLink>
           <NavLink to="/">Terris & Company</NavLink>
           <NavLink to="/contact">Contact</NavLink>
