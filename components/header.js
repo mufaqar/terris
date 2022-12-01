3e6749548d11dfce4fafa36574e60af440d63bcd
Mufaqar Islam <mufaqar@gmail.com>
2022-12-01 18:33:02 +0500

    css

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 function NavLink({ to, children }) {
   return (
@@ -16,11 +17,17 @@ export default function Header() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <nav className="mb-[84px]">
-        <div className="xl:w-[70%] w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[1.25rem] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal mt-[9px] px-2 flex justify-between">
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/">Terris & Company</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+      <nav className="mb-[84px] md:mb-[168px]">
+        <div className="xl:w-[70%] w-full mx-auto lg:text-[1.063rem] lg:leading-[1.375rem] md:text-[0.938rem] md:leading-[24px] text-[0.813rem] leading-[1.125rem] font-AtlasGroteskRegular font-normal mt-[9px] px-2 flex md:flex-col justify-between">
+          <Link href="/about">
+            <a className="md:order-last"> About</a>
+          </Link>
+          <Link href="/">
+            <a className="md:order-first"> Terris & Company </a>
+          </Link>
+          <Link href="/contact">
+            <a> Contact</a>
+          </Link>
         </div>
       </nav>
     </>
